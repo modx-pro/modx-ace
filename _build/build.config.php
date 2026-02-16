9501d4b015678397bf5ae55ffee70b101d38d4b5
Ivan Bochkarev <ibochkarev@uma.tech>
2026-02-16 22:56:16 +0600

    Update .gitignore and increment package version to 1.9.6

--- _build/build.config.php
+++ _build/build.config.php
@@ -2,7 +2,7 @@
 
 define('PKG_NAME', 'Ace');
 define('PKG_NAMESPACE', 'ace');
-define('PKG_VERSION', '1.9.5');
+define('PKG_VERSION', '1.9.6');
 define('PKG_RELEASE', 'pl');
 define('PKG_AUTO_INSTALL', true);
 
